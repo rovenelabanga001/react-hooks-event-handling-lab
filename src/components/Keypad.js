d0132e9ed9d4680e55b07904fef9db380dcc364e
rovenelabanga <rovenelabanga2001@gmail.com>
2024-11-02 18:22:57 +0300

    Uses events in React

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,21 @@
 // Code Keypad Component Here
+import React from "react";
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  function handleChange() {
+    console.log("Entering password...");
+  }
+
+  return (
+    <div>
+      <input
+        type="password"
+        name="Enter Password"
+        onChange={handleChange}
+        placeholder="Enter Password"
+      ></input>
+    </div>
+  );
 }
 
 export default Keypad;
